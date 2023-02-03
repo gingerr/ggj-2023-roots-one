be2c5befa03c228bd0d0a16ac7f74f68264f2cde
plumstone <37106962+plumstone@users.noreply.github.com>
2023-02-04 00:42:35 +0100

    Shooting is working now

--- Bullet.gd
+++ Bullet.gd
@@ -4,8 +4,8 @@ extends Area2D
 var velocity = Vector2(1000, 0)
 
 func _ready():
-	position.x = 100
-	position.y = get_viewport_rect().size.y / 2
+	#position.x = 100
+#	position.y = get_viewport_rect().size.y / 2
 	scale.x = 0.01
 	scale.y = 0.01
 
@@ -13,9 +13,17 @@ func _physics_process(delta):
 	position += velocity * delta
 
 func _process(delta):
+	if is_outside_view_bounds():
+		print("bullet outside screen")
+		queue_free()
 	position += velocity * delta
 	rotation = velocity.angle()
 
+func is_outside_view_bounds():
+	return position.x>OS.get_screen_size().x or position.x<0.0\
+		or position.y>OS.get_screen_size().y or position.y<0.0
 
 func _on_BallisticBullet_body_entered(body):
-	queue_free()
+	print("bullet hit something")
+	if body != get_node(""):
+		queue_free()
--- Player.gd
+++ Player.gd
@@ -1,30 +1,36 @@
-extends Area2D
+extends KinematicBody2D
+
+signal shoot
 
 export var muzzle_velocity = 350
 
+onready var muzzle: Position2D = $Muzzle
+
+var bullet_factory = preload("res://Bullet.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 100
-	position.y = get_viewport_rect().size.y / 2
 	scale.x = 0.05
 	scale.y = 0.05
+	position.x = 100
+	position.y = get_viewport_rect().size.y / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var speed = 500 * delta
 	if (Input.is_action_pressed("ui_left") && position.x > 100):
-		move_local_x(-speed)
-	if (Input.is_action_pressed("ui_up") && position.y > 100):
-		move_local_y(-speed)
-	if (Input.is_action_pressed("ui_right") && position.x < 600):
-		move_local_x(speed)
-	if (Input.is_action_pressed("ui_down") && position.y < 500):
 		move_local_y(speed)
+	if (Input.is_action_pressed("ui_up") && position.y > 100):
+		move_local_x(-speed)
+	if (Input.is_action_pressed("ui_right") && position.x < 600):
+		move_local_y(-speed)
+	if (Input.is_action_pressed("ui_down") && position.y < 500):
+		move_local_x(speed)
 	if (Input.is_action_just_pressed("ui_select")):
 		shoot()
 		emit_signal("shoot", Bullet, rotation, position)
-		print("space")
+		print("shoot")
 	if (Input.get_mouse_button_mask() == 0x03):
 		print("Left and right mouse buttons pressed!")
 	if (Input.is_key_pressed(KEY_ESCAPE)):
@@ -32,8 +38,10 @@ func _process(delta):
 	return delta
 
 func shoot():
-	var b = Bullet.new()
-	owner.add_child(b)
-	b.transform = transform
-	b.velocity = b.transform.x * muzzle_velocity
-	b.show()
+	var b = bullet_factory.instance()
+	
+	b.global_position = muzzle.global_position
+	#b.transform = transform
+	get_tree().root.add_child(b)
+	#b.velocity = b.transform.x * muzzle_velocity
+	#b.show()
