b10646f5777437030a741f8485f0c8761e7cad54
Mariusz Bieniek <ginger000@gmail.com>
2023-02-04 23:05:32 +0100

    intro reads story from file

--- Intro.gd
+++ Intro.gd
@@ -1,7 +1,23 @@
 extends Node
 
+func _ready():
+	var text = load_text_file('res://resources/story.tres')
+	var node: Label = get_node('%Label')
+	node.set_text(text)
+
 func _input(event: InputEvent):
 	if event.get('pressed') and event.pressed:
 		get_tree().change_scene("res://Game.tscn")
 		
 	pass
+
+func load_text_file(path):
+	var f = File.new()
+	var err = f.open(path, File.READ)
+	if err != OK:
+		printerr("Could not open file, error code ", err)
+		return ""
+		
+	var stext = f.get_as_text()
+	f.close()
+	return stext
