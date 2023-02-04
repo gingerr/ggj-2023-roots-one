50e5f486e03a39ebdca37c18ebeb36882655adfe
plumstone <37106962+plumstone@users.noreply.github.com>
2023-02-04 16:34:39 +0100

    Options in menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -10,9 +10,9 @@ func _ready():
 
 func _process(delta):
 	if (get_tree().paused):
-		$Selection/Continue.disabled = false
+		$VBoxContainer/MarginContainer/Selection/Continue.disabled = false
 	else:
-		$Selection/Continue.disabled = true
+		$VBoxContainer/MarginContainer/Selection/Continue.disabled = true
 
 func _on_ButtonNewGame_pressed():
 	get_tree().paused = false
@@ -26,3 +26,13 @@ func _on_Exit_pressed():
 func _on_Continue_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://Game.tscn")
+
+
+func _on_Options_pressed():
+	$VBoxContainer/MarginContainer/OptionsMenu.visible = true
+	$VBoxContainer/MarginContainer/Selection.visible = false
+
+
+func _on_BackToMain_pressed():
+	$VBoxContainer/MarginContainer/OptionsMenu.visible = false
+	$VBoxContainer/MarginContainer/Selection.visible = true
