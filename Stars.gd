f1c710109aed66e4deacf35e7e3a2f2d1bc28c61
Dennis Schladebeck <dennis@dev.bredex.de>
2023-02-04 01:06:29 +0100

    .

--- Stars.gd
+++ Stars.gd
@@ -2,15 +2,39 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const NUMBER_OF_STARS = 300
+const draw_primitive_dummy = PoolVector2Array()
 
+var stars:PoolVector2Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	
+	for i in range(NUMBER_OF_STARS):
+		var pos_X = rand_range(1,get_viewport_rect().size.x)
+		var pos_Y = rand_range(1,get_viewport_rect().size.y)
+		stars.append(Vector2(pos_X, pos_Y))
 
+func _physics_process(delta):
+	for i in range(NUMBER_OF_STARS):
+		var intensity = float(i % 5 + 1) / 5 # 0.2, 0.4, 0.6, 0.8, 1.0
+		var star = stars[i]
+		star.x -= 100 * delta * intensity
+		if (star.x <= 0):
+			star.x = int(get_viewport_rect().size.x)
+			star.y = int(rand_range(1,get_viewport_rect().size.y))
+		stars[i] = Vector2(star.x, star.y)
+	update()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _draw():
+	for i in range(NUMBER_OF_STARS):
+		var intensity = float(i % 5 + 1) / 5 # 0.2, 0.4, 0.6, 0.8, 1.0
+		var star = stars[i]
+		var point = PoolVector2Array( [star] ) 
+		var color = PoolColorArray( [Color(intensity, intensity, intensity)] ) 
+		draw_primitive(point, color, draw_primitive_dummy) 
+		
+	
 #func _process(delta):
 #	pass
