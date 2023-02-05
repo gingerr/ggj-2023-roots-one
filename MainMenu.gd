9cbc00050b2057f9f3e3378cb278678b9c80267a
Mariusz Bieniek <ginger000@gmail.com>
2023-02-05 16:27:42 +0100

    .

--- MainMenu.gd
+++ MainMenu.gd
@@ -11,30 +11,30 @@ func _on_Exit_pressed():
 	get_tree().quit()
 
 func _ready():
-	Utils.default_font_color($VBoxContainer/copyright)
+	Utils.default_font_color($HBoxContainer/VBoxContainer/copyright)
 	visualize_selection()
 
 func _on_Options_pressed():
-	$VBoxContainer/MarginContainer/OptionsMenu.visible = true
-	$VBoxContainer/MarginContainer/Selection.visible = false
+	$HBoxContainer/VBoxContainer/MarginContainer/OptionsMenu.visible = true
+	$HBoxContainer/VBoxContainer/MarginContainer/Selection.visible = false
 
 func _on_BackToMain_pressed():
-	$VBoxContainer/MarginContainer/OptionsMenu.visible = false
-	$VBoxContainer/MarginContainer/Selection.visible = true
+	$HBoxContainer/VBoxContainer/MarginContainer/OptionsMenu.visible = false
+	$HBoxContainer/VBoxContainer/MarginContainer/Selection.visible = true
 
 func _input(event: InputEvent) -> void:
 	
 	if event is InputEventMouse: 
-		if $VBoxContainer/MarginContainer/Selection/NewGame.is_hovered():
+		if $HBoxContainer/VBoxContainer/MarginContainer/Selection/NewGame.is_hovered():
 			selectionIndex = 0;
-		elif $VBoxContainer/MarginContainer/Selection/Exit.is_hovered():
+		elif $HBoxContainer/VBoxContainer/MarginContainer/Selection/Exit.is_hovered():
 			selectionIndex = 1;
 	if event is InputEventKey and event.pressed and event.scancode == KEY_ENTER:
 		match selectionIndex:
 			0: 
-				$VBoxContainer/MarginContainer/Selection/NewGame.emit_signal("pressed")
+				$HBoxContainer/VBoxContainer/MarginContainer/Selection/NewGame.emit_signal("pressed")
 			1: 
-				$VBoxContainer/MarginContainer/Selection/Exit.emit_signal("pressed")
+				$HBoxContainer/VBoxContainer/MarginContainer/Selection/Exit.emit_signal("pressed")
 		
 	if event is InputEventKey and event.pressed and event.scancode == KEY_DOWN:
 		selectionIndex = min(selectionIndex + 1, 1)
@@ -44,12 +44,12 @@ func _input(event: InputEvent) -> void:
 		visualize_selection()
 
 func visualize_selection():
-	Utils.button_not_selected($VBoxContainer/MarginContainer/Selection/NewGame)
-	Utils.button_not_selected($VBoxContainer/MarginContainer/Selection/Options)
-	Utils.button_not_selected($VBoxContainer/MarginContainer/Selection/Exit)
+	Utils.button_not_selected($HBoxContainer/VBoxContainer/MarginContainer/Selection/NewGame)
+	Utils.button_not_selected($HBoxContainer/VBoxContainer/MarginContainer/Selection/Options)
+	Utils.button_not_selected($HBoxContainer/VBoxContainer/MarginContainer/Selection/Exit)
 	match selectionIndex:
 		0: 
-			Utils.button_selected($VBoxContainer/MarginContainer/Selection/NewGame)
-			$VBoxContainer/MarginContainer/Selection/NewGame
+			Utils.button_selected($HBoxContainer/VBoxContainer/MarginContainer/Selection/NewGame)
+			$HBoxContainer/VBoxContainer/MarginContainer/Selection/NewGame
 		1: 
-			Utils.button_selected($VBoxContainer/MarginContainer/Selection/Exit)
+			Utils.button_selected($HBoxContainer/VBoxContainer/MarginContainer/Selection/Exit)
