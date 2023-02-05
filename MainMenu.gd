3d1ec166cd3413db1a880ae68d6705134a38bde8
Dennis Schladebeck <dennis@dev.bredex.de>
2023-02-05 14:28:16 +0100

    .

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,15 +1,6 @@
 extends MarginContainer
 
-var menuItems = Array([])
-
 var selectionIndex = 0;
-
-
-func _process(delta):
-	if (get_tree().paused):
-		$VBoxContainer/MarginContainer/Selection/Continue.disabled = false
-	else:
-		$VBoxContainer/MarginContainer/Selection/Continue.disabled = true
 
 func _on_NewGame_pressed():
 	get_tree().paused = false
@@ -20,11 +11,6 @@ func _on_Exit_pressed():
 	get_tree().quit()
 
 func _ready():
-	menuItems = Array([
-		$VBoxContainer/MarginContainer/Selection/NewGame,
-		$VBoxContainer/MarginContainer/Selection/Options,
-		$VBoxContainer/MarginContainer/Selection/Exit,
-	])
 	Utils.default_font_color($VBoxContainer/copyright)
 	visualize_selection()
 
@@ -37,20 +23,39 @@ func _on_BackToMain_pressed():
 	$VBoxContainer/MarginContainer/Selection.visible = true
 
 func _input(event: InputEvent) -> void:
+	
+	if event is InputEventMouse: 
+		if $VBoxContainer/MarginContainer/Selection/NewGame.is_hovered():
+			selectionIndex = 0;
+		elif $VBoxContainer/MarginContainer/Selection/Options.is_hovered():
+			selectionIndex = 1;
+		elif $VBoxContainer/MarginContainer/Selection/Exit.is_hovered():
+			selectionIndex = 2;
 	if event is InputEventKey and event.pressed and event.scancode == KEY_ENTER:
-		_on_NewGame_pressed()
+		match selectionIndex:
+			0: 
+				$VBoxContainer/MarginContainer/Selection/NewGame.emit_signal("pressed")
+			1: 
+				$VBoxContainer/MarginContainer/Selection/Options.emit_signal("pressed")
+			2: 
+				$VBoxContainer/MarginContainer/Selection/Exit.emit_signal("pressed")
+		
 	if event is InputEventKey and event.pressed and event.scancode == KEY_DOWN:
-		selectionIndex = min(selectionIndex + 1, menuItems.size() - 1)
+		selectionIndex = min(selectionIndex + 1, 2)
 		visualize_selection()
 	if event is InputEventKey and event.pressed and event.scancode == KEY_UP:
 		selectionIndex = max(selectionIndex - 1, 0)
 		visualize_selection()
 
 func visualize_selection():
-	print(selectionIndex)
-	for i in range(menuItems.size()):
-		if i == selectionIndex:
-			Utils.button_selected(menuItems[selectionIndex])
-		else:
-			Utils.button_not_selected(menuItems[selectionIndex])
-		update()
+	Utils.button_not_selected($VBoxContainer/MarginContainer/Selection/NewGame)
+	Utils.button_not_selected($VBoxContainer/MarginContainer/Selection/Options)
+	Utils.button_not_selected($VBoxContainer/MarginContainer/Selection/Exit)
+	match selectionIndex:
+		0: 
+			Utils.button_selected($VBoxContainer/MarginContainer/Selection/NewGame)
+			$VBoxContainer/MarginContainer/Selection/NewGame
+		1: 
+			Utils.button_selected($VBoxContainer/MarginContainer/Selection/Options)
+		2: 
+			Utils.button_selected($VBoxContainer/MarginContainer/Selection/Exit)
