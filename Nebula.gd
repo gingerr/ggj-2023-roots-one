86ec92b755aa061710cae15dbf6cdfbd7b35650a
Dennis Schladebeck <dennis@dev.bredex.de>
2023-02-05 02:11:08 +0100

    .

--- Nebula.gd
+++ Nebula.gd
@@ -1,15 +1,11 @@
 extends Sprite
 
-
 var bullet_tex3 = preload("res://resources/nebulaSmall.png")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = rand_range(1,get_viewport_rect().size.x)
 	position.y = rand_range(50,get_viewport_rect().size.y - 50)
 	texture = bullet_tex3
-	pass # Replace with function body.
 
 func _physics_process(delta):
 	position.x -= 10 * delta
@@ -18,6 +14,3 @@ func _physics_process(delta):
 	if (position.x <= -50):
 			position.x = int(get_viewport_rect().size.x) + 50
 			position.y = rand_range(50,get_viewport_rect().size.y - 50)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
