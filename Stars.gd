9b3894519313196e84962d5381fdfe265b530e06
Dennis Schladebeck <dennis@dev.bredex.de>
2023-02-07 14:57:00 +0100

    .

--- Stars.gd
+++ Stars.gd
@@ -1,18 +1,14 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-const NUMBER_OF_STARS = 10000
-const singleton_vector = Vector2(1, 1)
-const singleton_color = Color(1, 1, 1)
+const NUMBER_OF_STARS = 500
 const horzontal_movement_speed = 100
-const intensity_map = {
-	0 : 0.2,
-	1 : 0.4,
-	2 : 0.6,
-	3 : 0.8,
-	4 : 1.0
-}
+const brightnessArray = [
+	Color(0.2, 0.2, 0.2),
+	Color(0.4, 0.4, 0.4),
+	Color(0.6, 0.6, 0.6),
+	Color(0.8, 0.8, 0.8),
+	Color(1.0, 1.0, 1.0),
+]
 const intensity_map_with_speed = {
 	0 : 0.2 * horzontal_movement_speed,
 	1 : 0.4 * horzontal_movement_speed,
@@ -20,46 +16,46 @@ const intensity_map_with_speed = {
 	3 : 0.8 * horzontal_movement_speed,
 	4 : 1.0 * horzontal_movement_speed
 }
-const star_paint_width = 2.0
-
-var stars:PoolVector2Array = []
+var stars		= []
+var colors 	= []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(stars.size())
 	randomize()
-	
+	var vector = Vector2(0, 0)
 	for i in range(NUMBER_OF_STARS):
-		var pos_X = rand_range(1,get_viewport_rect().size.x)
-		var pos_Y = rand_range(1,get_viewport_rect().size.y)
-		stars.append(Vector2(pos_X, pos_Y))
+		var pos_X = rand_range(1, get_viewport_rect().size.x)
+		var pos_Y = rand_range(1, get_viewport_rect().size.y)
+		
+		vector.x = pos_X
+		vector.y = pos_Y
+		stars.append(vector)
+		
+		vector.x = pos_X + 2
+		vector.y = pos_Y
+		stars.append(vector)
+		
+		var brightness = brightnessArray[i % 5]
+		colors.append(brightness)
+		colors.append(brightness)
 
 func _physics_process(delta):
-	var end_of_screen = int(get_viewport_rect().size.x)
+	var screen_size = get_viewport_rect().size
 	for i in range(NUMBER_OF_STARS):
-		# performance optimization, lookup instead of float(i % 5 + 1) / 5
-		var intensity = intensity_map_with_speed[i % 5]
-		var star = stars[i]
-		# performance optimization:  instead of speed * delta * intensity, pre multiplied intensity * speed
-		star.x -= delta * intensity
-		if (star.x <= 0):
-			star.x = end_of_screen
-			star.y = int(rand_range(1,get_viewport_rect().size.y))
-		# Performance optimization, array object must be reset, but even same object works.. 
-		# creation of new object would be slower stars[i] = Vector2(star.x, star.y)
-		stars[i] = star 
-
-	# performance optimization => less redraw
-	if (Engine.get_physics_frames() % 3) == 0:
-		update()
+		var double_i = i * 2
+		var speed = intensity_map_with_speed[i % 5]
+		var from = stars[double_i]
+		from.x -= delta * speed
+		if (from.x <= 0):
+			from.x = screen_size.x
+			from.y = rand_range(1, screen_size.y)
+		var to = stars[double_i + 1]
+		to.x = from.x + 2
+		to.y = from.y
+		stars[double_i] 		= from
+		stars[double_i + 1] 	= to
+	update()
 
 func _draw():
-	for i in range(NUMBER_OF_STARS):
-		# performance optimization, lookup instead of float(i % 5 + 1) / 5
-		var intensity = intensity_map[i % 5]
-		var star = stars[i]
-		singleton_color.r = intensity 
-		singleton_color.g = intensity 
-		singleton_color.b = intensity 
-		singleton_vector.x = star.x + 2
-		singleton_vector.y = star.y
-		draw_line(star, singleton_vector, singleton_color, star_paint_width, false) 
+	draw_multiline_colors(stars, colors, 1.0, false) 
