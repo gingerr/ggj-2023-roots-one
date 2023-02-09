2094589661ea7d9efd35496962d0bce9543ef4cd
Dennis Schladebeck <dennis@dev.bredex.de>
2023-02-09 17:44:51 +0100

    .

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675960121|19899052858";
+const CACHE_VERSION = "1675961080|88137697";
 const CACHE_PREFIX = "shRooter-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
