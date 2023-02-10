1c58de7b3a06f385bb5ff36b657ac6f5cdfa2b82
Dennis Schladebeck <dennis@dev.bredex.de>
2023-02-10 19:03:50 +0100

    .

--- Utils.gd
+++ Utils.gd
@@ -7,12 +7,12 @@ static func randomBoolean():
 	return false
 	
 static func default_font_color(label : Label):
-	label.set("custom_colors/font_color", Color(1,0.7,0.0))
+	label.set("theme_override_colors/font_color", Color(1,0.7,0.0))
 
 static func button_selected(label : Button):
-	label.set("custom_colors/font_color", Color(1.0,0.7,0.0))
-	label.set("custom_colors/font_color_hover", Color(1.0,0.7,0.0))
+	label.set("theme_override_colors/font_color", Color(1.0,0.7,0.0))
+	label.set("theme_override_colors/font_color_hover", Color(1.0,0.7,0.0))
 
 static func button_not_selected(label : Button):
-	label.set("custom_colors/font_color", Color(1,1,1))
-	label.set("custom_colors/font_color_hover", Color(1,1,1))
+	label.set("theme_override_colors/font_color", Color(1,1,1))
+	label.set("theme_override_colors/font_color_hover", Color(1,1,1))
