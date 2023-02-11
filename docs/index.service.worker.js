d8ea5636ac8cf2ec25fe68558997ee058a75867c
Mariusz Bieniek <ginger000@gmail.com>
2023-02-11 01:32:50 +0100

    .

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1676075149|285937109";
+const CACHE_VERSION = "1676075534|53418931";
 const CACHE_PREFIX = "shRooter-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
